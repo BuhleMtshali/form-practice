9cf9a3fcd56f72ddb93cd88640db3fcd1cbbc254
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-03 23:01:14 +0200

    updated the js file

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const originalForm = document.querySelector("#input-el").innerHTML;
 
+function popUp(event) {
+  event.preventDefault();
+  let inputElement = document.querySelector("#input-el");
+  inputElement.innerHTML = "You've Checked in Successfully!!";
+  let buttonElement = document.querySelector("#submit");
+  buttonElement.textContent = "Done";
+  buttonElement.removeEventListener("click", popUp);
+  buttonElement.addEventListener("click", restoreForm);
+}
+
+function restoreForm(event) {
+  event.preventDefault();
+  let inputElement = document.querySelector("#input-el");
+  inputElement.innerHTML = originalForm;
+  let buttonElement = document.querySelector("#submit");
+  buttonElement.textContent = "Submit";
+  buttonElement.removeEventListener("click", restoreForm); // Remove the restoreForm event listener
+  buttonElement.addEventListener("click", popUp); // Add the popUp event listener
+}
+
+let buttonElement = document.querySelector("#submit");
+buttonElement.addEventListener("click", popUp);
